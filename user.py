48c178732860a91055333e076964f1139c85d5b1
Anton <vip.valko@gmail.com>
2018-11-12 00:50:24 +0200

    create class User with expense and income functions

---
+++ user.py
@@ -0,0 +1,36 @@
+class User:
+    def __init__(self, name, user_id):
+        self.name = name
+        self.user_id = user_id
+        self.incomes = {}
+        self.expenses = {}
+
+    def new_expense(self, date, money, category, comment=None):
+        """ Добавить новый расход """
+        self.expenses[len(self.expenses)+1] = Expense(date, money, category, comment)
+
+    def new_income(self, date, money, comment=None):
+        """ Добавить новый доход """
+        self.incomes[len(self.incomes)+1] = Income(date, money, comment)
+
+    def __str__(self):
+        """ Красиво вывести объект """
+        return f'< {self.__class__.__name__} - {self.name} (id={self.user_id}) >'
+
+
+if __name__ == '__main__':
+    from bot_cash.cash_flow import Expense, Income
+
+    new_user = User('Anton', 247)
+
+    print(new_user)
+    print(new_user.incomes)
+    print(new_user.expenses)
+
+    new_user.new_income('12.11.2018', 300, 'зп за урок пайтона')
+    new_user.new_income('19.11.2018', 300, 'зп за урок пайтона')
+
+    print(new_user.incomes[1])
+    print(new_user.incomes[2])
+
+    print(new_user.incomes)
